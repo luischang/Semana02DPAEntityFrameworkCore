223135ca717f2eebff259cc63373000a631d57ba
Luis Chang <luis.chang@outlook.com>
2022-09-04 14:28:49 -0500

    Agregar archivos de proyecto.

---
+++ Semana02DPAConsole.CodeFirst/Models/Qatar2022DbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Semana02DPAConsole.CodeFirst.Models
+{
+    public class Qatar2022DbContext
+    {
+
+
+
+    }
+}
