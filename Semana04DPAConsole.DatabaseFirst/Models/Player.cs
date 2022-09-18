bd7aee43756347e47163a80fbfbc61d749c4609f
Luis Chang <luis.chang@outlook.com>
2022-09-18 12:24:07 -0500

    Database first Player-Team

---
+++ Semana04DPAConsole.DatabaseFirst/Models/Player.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Semana04DPAConsole.DatabaseFirst.Models
+{
+    public partial class Player
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; } = null!;
+        public int Dorsal { get; set; }
+        public bool? Status { get; set; }
+        public int? TeamId { get; set; }
+        public string? Country { get; set; }
+
+        public virtual Team? Team { get; set; }
+    }
+}
---
+++ Semana04DPAConsole.DatabaseFirst/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Semana04DPAConsole.DatabaseFirst.Models;
+
+Console.WriteLine("Hello, World!");
+
+var context = new Qatar2022DbContext();
+
+//Insert player
+var player1 = new Player()
+{
+    FullName = "Raul Ruidíaz",
+    Dorsal = 11,
+    Status = true
+};
+
+context.Player.Add(player1);
+context.SaveChanges();
